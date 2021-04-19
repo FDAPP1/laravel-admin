53e50991d7f99c7c6fa2fd712cecc18219711626
FDAPP1 <76927922+kanazawaDF@users.noreply.github.com>
2021-04-19 09:57:19 +0900

    フォーム内にCSRFトークンの記述2 metaタグ

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
   <title>{{config('admin.title')}} | {{ trans('admin.login') }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   @if(!is_null($favicon = Admin::favicon()))
   <link rel="shortcut icon" href="{{$favicon}}">
   @endif
